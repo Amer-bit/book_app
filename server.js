548095ef9e4743f6f2a6d40f82712313766c2899
Amer-bit <ny.1995@outlook.com>
2020-04-14 01:29:36 +0300

    CSS left and minor stuff

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 /////////////////////////////////Requiered all dependencies (Setting up the app)////////////////////////
-require('dotenv').config();
-const express = require('express')
-const pg = require('pg');
-const cors = require('cors');
-const superagent = require('superagent');
+require("dotenv").config();
+const express = require("express");
+const pg = require("pg");
+const cors = require("cors");
+const superagent = require("superagent");
 const PORT = process.env.PORT || 4000;
 
 ///////////////////////////// Initializing the Server////////////////////////////
@@ -13,86 +13,104 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 ///////////////////////////////
 app.use(cors());
-app.set('view engine', 'ejs');
-app.use(express.static('./public'))
+app.set("view engine", "ejs");
+app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: true }));
 
 ///////////////////////////// Routes Definition and routes handlers \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-app.get('/', getBooks)
-app.get('/add', (req, res) => {    
-    res.render( 'pages/searches/new')
-})
+app.get("/", getBooks);
+app.get("/add", (req, res) => {
+  res.render("pages/searches/new");
+});
 
-app.post('/searches', (req, res) => {
-    let url;
-    const userInput = req.body.bookSearch;
-    const searchType = req.body.searchType;
-    console.log('in',req.body);
+app.post("/searches", searchHandler);
 
-    if(searchType === "title"){
-        url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${userInput}`
-    } else{
-        url = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${userInput}`
-    }
-    ////// or we can use one URL that take two values`https://www.googleapis.com/books/v1/volumes?q=in${searchType}:${userInput}`
+app.get("/books/:bookid", specificBook);
 
-    superagent.get(url)
-    .then(googleBooksData => {
-        // console.log(googleBooksData.body.items);
-        const googleLibrary = googleBooksData.body.items;
-        // console.log(googleLibrary);
-        
-        let googleBooks = googleLibrary.map( value => {
-           let creatingBookIns =  new Book (userInput, value);
-           const SQL = 'INSERT INTO gobooks(img_url, title, author, description) VALUES ($1,$2,$3,$4) RETURNING *;'
-           let safeValues = Object.values(creatingBookIns)
-        //    console.log("HELLLLLLLLLLLOOOOOOOOOOOOO",safeValues);
+app.post("/books", collection);
 
-        client.query(SQL, safeValues)
-        .then(data =>{
-            // console.log(data);
-        })
-          return creatingBookIns
-        })    
-        res.render('pages/searches/show', {boooks:googleBooks})
-        // res.status(200).json(googleLibrary)
-        
-    })
-} )
+//////////////////////////////////////// Route Handler \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+function searchHandler (req, res) {
+  let url;
+  const userInput = req.body.bookSearch;
+  const searchType = req.body.searchType;
+  if (searchType === "title") {
+    url = `https://www.googleapis.com/books/v1/volumes?q=intitle:${userInput}`;
+  } else {
+    url = `https://www.googleapis.com/books/v1/volumes?q=inauthor:${userInput}`;
+  }
+  ////// or we can use one URL that take two values`https://www.googleapis.com/books/v1/volumes?q=in${searchType}:${userInput}`
+  superagent.get(url).then((googleBooksData) => {
+    const googleLibrary = googleBooksData.body.items;
+
+    let googleBooks = googleLibrary.map((value) => {
+      let creatingBookIns = new Book(userInput, value);
+      return creatingBookIns;
+    });
+    res.render("pages/searches/show", { boooks: googleBooks });
+  });
+}
+
+
+function getBooks(req, res) {
+  const SQL = "SELECT * FROM goBooks; ";
+  client.query(SQL).then((data) => {
+    // console.log(data);
+
+    res.render("pages/index", { sqldata: data.rows });
+  });
+}
+
+
+function specificBook(req, res) {
+  const SQL = "SELECT * FROM gobooks WHERE id=$1;";
+  let safeValues = [req.params.bookid];
+  client.query(SQL, safeValues).then((data) => {
+    // console.log(data.rows);
+    res.render("pages/books/detail", { partbook: data.rows[0] });
+  });
+  // console.log(req.params);
+}
+
+
+function collection(req, res) {
+  let addBook = req.body.add;
+  const SQL =
+    "INSERT INTO gobooks(img_url, title, author, description) VALUES ($1,$2,$3,$4) RETURNING *;";
+  let safeValues = [addBook[0], addBook[1], addBook[2], addBook[3]];
+  ////////////// OR we can use object deconstructing like {img, author, title, overview} = creatingBookIns
+  ///////////// they must be the same name as the properties we can accsess the properties without obj.propertiy
+  client.query(SQL, safeValues).then(() => {
+    res.status(200).redirect("/");
+  });
+}
 
 ////////////////////////////// Constructor \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-function Book (userInput, googleApiRes){
-    this.img = googleApiRes.volumeInfo.imageLinks && googleApiRes.volumeInfo.imageLinks.thumbnail || `https://via.placeholder.com/250x300/000000`;
-    this.title = googleApiRes.volumeInfo && googleApiRes.volumeInfo.title || userInput;
-    this.author = googleApiRes.volumeInfo.authors || userInput;
-    this.overview = googleApiRes.volumeInfo.description;
-    // this.isbn;
-    // this.bookshelf;
+function Book(userInput, googleApiRes) {
+  this.img =
+    (googleApiRes.volumeInfo.imageLinks &&
+      googleApiRes.volumeInfo.imageLinks.thumbnail) ||
+    `https://via.placeholder.com/250x300/000000`;
+  this.title =
+    (googleApiRes.volumeInfo && googleApiRes.volumeInfo.title) || userInput;
+  this.author = googleApiRes.volumeInfo.authors || userInput;
+  this.overview = googleApiRes.volumeInfo.description;
+  // this.isbn;
+  // this.bookshelf;
 }
 
-function getBooks(req, res){
-    const SQL = 'SELECT * FROM goBooks; ';
-    client
-    .query(SQL)
-    .then(data => {
-        console.log(data);
-        
-        res.render('pages/index', {sqldata : data.rows});
-    })
+function errorHandler(req, res, error) {
+  res.status(500).json(error);
 }
-
-function errorHandler(req, res, error){
-    res.status(500).json(error)
-}
-
 
 client
-.connect()
-.then(() => {
-    app.listen(PORT , console.log(`Up and running on ${PORT}`))
-}).catch((error =>{
+  .connect()
+  .then(() => {
+    app.listen(PORT, console.log(`Up and running on ${PORT}`));
+  })
+  .catch((error) => {
     throw new Error(`startup error ${error}`);
-}))
+  });
